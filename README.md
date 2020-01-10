fec5d4a025d0f7f568ecb4b08883292ee00c6643
Marcelo Pinheiro <mpinheir@gmail.com>
2020-01-09 20:19:29 -0800

    Added README.md to project.

---
+++ README.md
@@ -0,0 +1,16 @@
+# Conversor de temperatura
+
+O objetivo deste projeto é exemplificar a criação de rotinas de conversão de temperatura em Python.
+
+## Prerequisitos
+
+Python 3.*
+
+## Author
+
+[**Marcelo Pinheiro**](https://github.com/mpinheir)
+
+## License
+
+Copie e use como desejado.
+
--- celsius_to_f.py
+++ celsius_to_f.py
@@ -1,6 +1,5 @@
 """Programa para converter temperatura em Celsius para Farenheit"""
 
-
 def c2f(celsius):
     '''Conversao de Celsius para Farenheint'''
     return (9.0 / 5.0) * celsius + 32
@@ -10,7 +9,6 @@ def exib_f(celsius):
     print (celsius, "celsius =", c2f(celsius), 'Farenheit')
 
 # executa funcao para converter 40, 20 e -40 Celsius para o equivalente em Farenheit
-
 exib_f(40)
 exib_f(20)
 exib_f(-40)
