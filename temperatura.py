9840fc3946f346154128ceef7068d63d93659c44
samuelbartels20 <bartelssamuel20@gmail.com>
2020-01-19 21:38:09 +0000

    adding change

--- temperatura.py
+++ temperatura.py
@@ -13,17 +13,32 @@
 # exib_f(20)
 # exib_f(-40)
 
-temp = input("Input the  temperature you like to convert? (e.g., 45F, 102C etc.) : ")
-degree = int(temp[:-1])
-i_convention = temp[-1]
 
-if i_convention.upper() == "C":
-  result = int(round((9 * degree) / 5 + 32))
-  o_convention = "Fahrenheit"
-elif i_convention.upper() == "F":
-  result = int(round((degree - 32) * 5 / 9))
-  o_convention = "Celsius"
-else:
-  print("Input proper convention.")
-  quit()
-print("The temperature in", o_convention, "is", result, "degrees.")
+
+
+def menu():
+        print("\n1. Celsius to Fahrenheit")
+        print("2. Fahrenheit to Celsius")
+        print("3. Exit")
+        pick = int(input("Enter a choice: "))
+        return pick
+def toCelsius(f):
+        return int((f - 32) * 5.0 / 9.0)
+def tofahrenheit(c):
+        return int((9.0 / 5.0) * c + 32)
+def main():
+        choice = menu()
+        
+        while choice != 3:
+                if choice == 1:
+                        #convert C to F
+                        c = eval(input("Enter degrees Celsius: "))
+                        print(str(c) + "C= " + str(toFahrenheit(c)) + "F")
+                elif choice == 2:
+                        #convert F to C
+                        f = eval(input("Enter degrees Fahrenheit: "))
+                        print(str(f) + "F = " + str(toCelsius(f)) + "C")
+                else:
+                        print("Invalid entry")
+                choice = menu()
+main()
