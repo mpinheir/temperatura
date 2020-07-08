430cd5b8af773aeddaca575fd2c27a8672f33f3c
vzamith <65679872+vzamith@users.noreply.github.com>
2020-07-08 20:07:15 +0200

    Adicionando funções para conversão de kelvin pra celsius

--- exemplo_temperatura.py
+++ exemplo_temperatura.py
@@ -5,8 +5,22 @@ import temperatura
 def exib_f(celsius):
     '''imprime conversao de temperatura'''
     print (celsius, "celsius =", temperatura.c2f(celsius), 'Farenheit')
+def exib_k(celsius):
+    '''imprime conversao de temperatura'''
+    print (celsius, "celsius=", temperatura.c2k(celsius), 'Kelvin')
 
 # executa funcao para converter 40, 20 e -40 Celsius para o equivalente em Farenheit
 exib_f(40)
 exib_f(20)
 exib_f(-40)
+
+# executa funcao para converter 50, 10, 0 Celsius para o equivalente em Kelvin
+
+exib_k(50)
+exib_k(10)
+exib_k(0)
+
+
+
+
+
