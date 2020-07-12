546cf85c19e6e05348d56366a8742bdcf8616352
vzamith <65679872+vzamith@users.noreply.github.com>
2020-07-13 01:52:09 +0200

    Add Kelvin conversions and change name of exib functions. (#3)

--- temperatura.py
+++ temperatura.py
@@ -8,3 +8,10 @@ def f2c(faren):
     '''Conversao de Farenheint para Celsius'''
     return (faren - 32) * (5.0 / 9.0)
 
+def c2k(celsius):
+    '''Conversao de Celsius para Kelvin'''
+    return celsius + 273.15
+
+def k2c(kelvin):
+    '''Conversao de Kelvin para Celsius'''
+    return kelvin - 273.15
