ff3ae0e93ea9938f8c1f87bd858e172950f4f0bf
vzamith <65679872+vzamith@users.noreply.github.com>
2020-07-12 19:52:15 +0200

    Add Kelvin conversions and change name of exib functions.

--- exemplo_temperatura.py
+++ exemplo_temperatura.py
@@ -2,24 +2,35 @@
 
 import temperatura
 
-def exib_f(celsius):
+def exib_c2f(celsius):
     '''imprime conversao de temperatura de celsius para farenheit'''
-    print (celsius, "celsius =", temperatura.c2f(celsius), 'Farenheit')
-def exib_F(faren):
+    print(celsius, "celsius =", temperatura.c2f(celsius), 'Farenheit')
+def exib_f2c(faren):
     '''imprime conversao de temperatura de farenheit pare celsius'''
-    print (faren, "Farenheit=", temperatura.f2c(faren), 'Celsius')
+    print(faren, "Farenheit =", temperatura.f2c(faren), 'Celsius')
+def exib_c2k(celsius):
+    '''imprime conversao de temperatura de celsius para kelvin'''
+    print(celsius, "Celsius =", temperatura.c2k(celsius), 'Kelvin')
+def exib_k2c(kelvin):
+    '''imprime conversao de temperatura de kelvin para celsius'''
+    print(kelvin, "Kelvin =", temperatura.k2c, "Celsius")
 
 # executa funcao para converter 40, 20 e -40 Celsius para o equivalente em Farenheit
-exib_f(40)
-exib_f(20)
-exib_f(-40)
+exib_c2f(40)
+exib_c2f(20)
+exib_c2f(-40)
 
 # executa funcao para converter 32, -40, 0 Farenheit para o equivalente em Celsius
-exib_F(32)
-exib_F(-40)
-exib_F(0)
+exib_f2c(32)
+exib_f2c(-40)
+exib_f2c(0)
 
+# executa funcao para converter 40, 20, -40 Celsius para o equivalente em Kelvin
+exib_c2k(40)
+exib_c2k(20)
+exib_c2k(-40)
 
-
-
-
+# executa funcao para converter 0, 273.15, 65 Kelvin para o equivalente em Celsius
+exib_k2c(0)
+exib_k2c(273.15)
+exib_k2c(373.15)
--- test_temperatura.py
+++ test_temperatura.py
@@ -9,3 +9,11 @@ def test_c2f():
 def test_f2c():
     """#Test da funcao que converte farenheint para graus celsius"""
     assert temperatura.f2c(32) == 0
+
+def test_c2k():
+    """#Test da funcao que converte celsius para kelvin"""
+    assert temperatura.c2k(0) == 273.15
+
+def test_k2c():
+    """#Test da funcao que converte kelvin para celsius"""
+    assert temperatura.k2c(273.15) == 0
